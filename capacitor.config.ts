e5f94d87122e4e34e7756fe8b4f89c5c00462401
spacecityx <shawn@spacecityx.com>
2026-03-31 10:42:09 -0600

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,17 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spacecityx.findmyapple',
+  appName: 'Find My Apple',
+  webDir: 'dist',
+  server: { androidScheme: 'https' },
+  android: {
+    allowMixedContent: false,
+    backgroundColor: '#0a0c12'
+  },
+  plugins: {
+    Browser: { presentationStyle: 'fullscreen' }
+  }
+};
+
+export default config;
