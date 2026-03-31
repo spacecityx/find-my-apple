9487af150b49e105ecf3e0ba334b8f71f2bd7add
spacecityx <shawn@spacecityx.com>
2026-03-31 10:50:43 -0600

    Add src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { Route, Switch } from 'wouter'
 import Home from './pages/Home'
-
 export default function App() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-    </Switch>
-  )
+  return <Switch><Route path="/" component={Home} /></Switch>
 }
